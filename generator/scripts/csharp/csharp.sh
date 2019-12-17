5c96ae7ae01d314ded9babfe7eb4adea9639f6f3
kensan23 <9150185+kensan23@users.noreply.github.com>
2019-12-18 10:09:56 +1100

    Fix folder

--- generator/scripts/csharp/csharp.sh
+++ generator/scripts/csharp/csharp.sh
@@ -2,4 +2,4 @@
 set -euxo pipefail
 
 cp /usr/local/bin/docker-entrypoint.sh /usr/local/bin/openapi-generator
-./generator/scripts/dotnet-oauth2-accounting.sh
+./generator/scripts/csharp/dotnet-oauth2-accounting.sh
