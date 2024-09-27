bdf42012d7fe02d53cdbbd91708a43761a245f9d
sangeet-joy_xero <sangeet.joy@xero.com>
2024-09-27 15:45:24 +0530

    Feat: Added the semantic release code

---
+++ .github/semantic-release/release.config.js
@@ -0,0 +1,21 @@
+const config = {
+    branches: ['master'],
+    Plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/changelog',
+        '@semantic-release/github',
+        [
+            '@semantic-release/git',
+            {
+                "assests": ["CHANGELOG.md"],
+                "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+            }
+        ],
+        ["@semantic-release/npm", {
+            "npmPublish": false,
+        }]
+    ]
+}
+
+module.exports = config
