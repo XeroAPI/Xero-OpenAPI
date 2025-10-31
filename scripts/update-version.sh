90410d1638eb0a9e0b1ccc88a9c828ea3eaf1fc1
Chris Mitchell <chris.t.mitchell@gmail.com>
2025-10-31 15:49:07 +1300

    [PETOSS-829] Increment version using script from exec hook instead of separate workflow (#769)

---
+++ scripts/update-version.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+versionNumber=${1:?Version number is required as first argument}
+branchName=${2:-master}
+
+git checkout "$branchName"
+
+for file in xero*.yaml; do
+    yq eval --no-colors --prettyPrint ".info.version = \"$versionNumber\"" -i "$file"
+    echo "updated version in $file to $versionNumber"
+done
+
+git add xero*.yaml
+git commit -m "chore: bump version to $versionNumber"
+git push origin "$branchName"
